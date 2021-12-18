72d956c989d80a6a3375101d3fba8f469b925d4f
Jeremiah Adsit <jeremiah.adsit@gmail.com>
2021-12-17 22:26:19 -0700

    production deployment

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -3,7 +3,7 @@
  * The default values is overridden by the `API_BASE_URL` environment variable.
  */
  
- const API_BASE_URL = "https://jma-reservation-backend.herokuapp.com/";
+ const API_BASE_URL = "https://jma-reservation-backend.herokuapp.com";
   //  process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
  
  /**
